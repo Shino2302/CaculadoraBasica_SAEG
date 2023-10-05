615d3e4360580d52aa8eb31e5a3633084ee902d7
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2023-10-05 06:56:02 -0700

    si

--- CaculadoraBasica_SAEG/Funciones/Funcionalidades.cs
+++ CaculadoraBasica_SAEG/Funciones/Funcionalidades.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace CaculadoraBasica_SAEG.Funciones
@@ -23,9 +24,55 @@ namespace CaculadoraBasica_SAEG.Funciones
             }
             return respuesta;
         }
-        public bool EsDouble(string resultado) 
+        public bool TieneOperadores(string resultado) 
         {
-
+            bool respuesta = false;
+            for(int i = 0; i < resultado.Length; i++)
+            {
+                if (resultado[i] != '+')
+                {
+                    respuesta = false;
+                }
+                else if (resultado[i] != '-')
+                {
+                    respuesta = false;
+                }
+                else if (resultado[i] != '*')
+                {
+                    respuesta = false;
+                }
+                else if (resultado[i] != '/')
+                {
+                    respuesta = false;
+                }
+                else if (resultado[i] != '%')
+                {
+                    respuesta = false;
+                }
+                else
+                {
+                    respuesta = true;
+                    break;
+                }
+            }
+            return respuesta;
+        }
+        public bool EsDivision(string conversion)
+        {
+            bool respuesta = false;
+            for(int i = 0; i < conversion.Length; i++)
+            {
+                if (conversion[i] != '/')
+                {
+                    respuesta = false;
+                }
+                else
+                {
+                    respuesta = true;
+                    break;
+                }
+            }
+            return respuesta;
         }
     }
 }
--- CaculadoraBasica_SAEG/MainPage.xaml.cs
+++ CaculadoraBasica_SAEG/MainPage.xaml.cs
@@ -12,19 +12,30 @@ namespace CaculadoraBasica_SAEG
     public partial class MainPage : ContentPage
     {
         string resultado = "";
-        string operaciones = "";
         string variableAuxiliar = "";
+        string conversion = "";
+        bool comprobar = false;
         public MainPage()
         {
             InitializeComponent();
         }
         public void Resultados()
         {
-            Resultado.Text = resultado;
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                Resultado.Text = resultado;
+            }
+            else
+            {
+                Resultado.Text = resultado + " " + variableAuxiliar;
+            }
         }
         private void Button_Clicked(object sender, EventArgs e)
         {
             resultado = "";
+            variableAuxiliar = "";
             Resultados();
         }
         private void Button_Clicked_Borrar(object sender, EventArgs e)
@@ -34,76 +45,244 @@ namespace CaculadoraBasica_SAEG
 
         private void Button_Clicked_0(object sender, EventArgs e)
         {
-            resultado += "0";
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += "0";
+            }
+            else
+            {
+                variableAuxiliar += "0";
+            }
             Resultados();
         }
 
         private void Button_Clicked_1(object sender, EventArgs e)
         {
-            resultado += "1";
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += "1";
+            }
+            else
+            {
+                variableAuxiliar += "1";
+            }
             Resultados();
         }
 
         private void Button_Clicked_2(object sender, EventArgs e)
         {
-            resultado += "2";
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false) 
+            { 
+                resultado += "2";
+            }
+            else
+            {
+                variableAuxiliar += "2";
+            }
             Resultados();
         }
 
         private void Button_Clicked_3(object sender, EventArgs e)
         {
-            resultado += "3";
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += "3";
+            }
+            else
+            {
+                variableAuxiliar += "3";
+            }
             Resultados();
         }
         private void Button_Clicked_4(object sender, EventArgs e)
         {
-            resultado += "4";
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += "4";
+            }
+            else
+            {
+                variableAuxiliar += "4";
+            }
             Resultados();
         }
         private void Button_Clicked_5(object sender, EventArgs e)
         {
-            resultado += "5";
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += "5";
+            }
+            else
+            {
+                variableAuxiliar += "5";
+            }
             Resultados();
         }
 
         private void Button_Clicked_6(object sender, EventArgs e)
         {
-            resultado += "6";
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += "6";
+            }
+            else
+            {
+                variableAuxiliar += "6";
+            }
             Resultados();
         }
 
         private void Button_Clicked_7(object sender, EventArgs e)
         {
-            resultado += "7";
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += "7";
+            }
+            else
+            {
+                variableAuxiliar += "7";
+            }
             Resultados();
         }
         private void Button_Clicked_8(object sender, EventArgs e)
         {
-            resultado += "8";
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += "8";
+            }
+            else
+            {
+                variableAuxiliar += "8";
+            }
             Resultados();
         }
         private void Button_Clicked_9(object sender, EventArgs e)
         {
-            resultado += "9";
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += "9";
+            }
+            else
+            {
+                variableAuxiliar += "9";
+            }
             Resultados();
         }
         private void Button_Point(object sender, EventArgs e)
         {
             Funcionalidades funcionalidades = new Funcionalidades();
-            if (!funcionalidades.EsDecimal(resultado))
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
             {
-                resultado += ".";
-                Resultados();
+                comprobar = funcionalidades.EsDecimal(resultado);
+                if (comprobar == false)
+                {
+                    resultado += ".";
+                }
             }
+            else
+            {
+                comprobar = funcionalidades.EsDecimal(variableAuxiliar);
+                if (comprobar == false)
+                {
+                    variableAuxiliar += ".";
+                }
+            }
+            Resultados();
         }
-
         private void Button_Clicked_Resultado(object sender, EventArgs e)
         {
-
+            Funcionalidades funcionalidades = new Funcionalidades();
+            if(variableAuxiliar.Length > 0)
+            {
+                conversion = resultado + variableAuxiliar;
+                if (funcionalidades.EsDecimal(resultado) 
+                    || funcionalidades.EsDecimal(variableAuxiliar)
+                    || funcionalidades.EsDivision(conversion))
+                {
+                    float operacionHecha = Convert.ToSingle(conversion);
+                    resultado = operacionHecha.ToString();
+                    variableAuxiliar = "";
+                    Resultados();
+                }
+                else 
+                {
+                    int operacionesHechas = Convert.ToInt32(conversion);
+                    resultado = operacionesHechas.ToString();
+                    variableAuxiliar = "";
+                    Resultados();
+                }
+            }
         }
         private void Button_Clicked_Mod(object sender, EventArgs e)
         {
-
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += " %";
+                Resultados();
+            }
+        }
+        private void Button_Clicked_Multi(object sender, EventArgs e)
+        {
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += " *";
+                Resultados();
+            }
+        }
+        private void Button_Clicked_Suma(object sender, EventArgs e)
+        {
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += " +";
+                Resultados();
+            }
+        }
+        private void Button_Clicked_Resta(object sender, EventArgs e)
+        {
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += " -";
+                Resultados();
+            }
+        }
+        private void Button_Clicked_Division(object sender, EventArgs e)
+        {
+            Funcionalidades funcionalidades = new Funcionalidades();
+            comprobar = funcionalidades.TieneOperadores(resultado);
+            if (comprobar == false)
+            {
+                resultado += " /";
+                Resultados();
+            }
         }
     }
 }
