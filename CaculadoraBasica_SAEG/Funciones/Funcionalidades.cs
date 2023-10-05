e9be043408ab350d8a15b2806eb5b86d01df4fdf
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2023-10-05 05:26:43 -0700

    Agregar archivos de proyecto.

---
+++ CaculadoraBasica_SAEG/Funciones/Funcionalidades.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaculadoraBasica_SAEG.Funciones
+{
+    class Funcionalidades
+    {
+        public bool EsDecimal(string resultado)
+        {
+            bool respuesta = false;
+            for(int i = 0; i < resultado.Length; i++)
+            {
+                if (resultado[i] != '.')
+                {
+                    respuesta = false;
+                }
+                else
+                {
+                    respuesta = true;
+                    break;
+                }
+            }
+            return respuesta;
+        }
+        public bool EsDouble(string resultado) 
+        {
+
+        }
+    }
+}
---
+++ CaculadoraBasica_SAEG/MainPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using CaculadoraBasica_SAEG.Funciones;
+
+namespace CaculadoraBasica_SAEG
+{
+    public partial class MainPage : ContentPage
+    {
+        string resultado = "";
+        string operaciones = "";
+        string variableAuxiliar = "";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void Resultados()
+        {
+            Resultado.Text = resultado;
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            resultado = "";
+            Resultados();
+        }
+        private void Button_Clicked_Borrar(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button_Clicked_0(object sender, EventArgs e)
+        {
+            resultado += "0";
+            Resultados();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            resultado += "1";
+            Resultados();
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            resultado += "2";
+            Resultados();
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+            resultado += "3";
+            Resultados();
+        }
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+            resultado += "4";
+            Resultados();
+        }
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+            resultado += "5";
+            Resultados();
+        }
+
+        private void Button_Clicked_6(object sender, EventArgs e)
+        {
+            resultado += "6";
+            Resultados();
+        }
+
+        private void Button_Clicked_7(object sender, EventArgs e)
+        {
+            resultado += "7";
+            Resultados();
+        }
+        private void Button_Clicked_8(object sender, EventArgs e)
+        {
+            resultado += "8";
+            Resultados();
+        }
+        private void Button_Clicked_9(object sender, EventArgs e)
+        {
+            resultado += "9";
+            Resultados();
+        }
+        private void Button_Point(object sender, EventArgs e)
+        {
+            Funcionalidades funcionalidades = new Funcionalidades();
+            if (!funcionalidades.EsDecimal(resultado))
+            {
+                resultado += ".";
+                Resultados();
+            }
+        }
+
+        private void Button_Clicked_Resultado(object sender, EventArgs e)
+        {
+
+        }
+        private void Button_Clicked_Mod(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
